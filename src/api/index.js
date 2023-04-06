34d4d757735a63afd3c5d1e9b9f1efc6b7da8222
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2023-04-06 23:22:59 +0900

    Get current date in API file

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+const baseUrl = "https://api.rawg.io/api";
+
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  return month < 10 ? `0${month}` : month;
+};
+
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  return day < 10 ? `0${day}` : day;
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+
+console.log(currentDate);
