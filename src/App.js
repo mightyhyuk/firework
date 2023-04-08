d8dd4f8604f7a818af5ab210af0c2ce4042a8e52
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2023-04-08 10:30:29 +0900

    Create Button by styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
 function App() {
   return (
     <div className="App">
       <h1>Hello Firework!</h1>
+      <Button>Normal Button</Button>
     </div>
   );
 }
